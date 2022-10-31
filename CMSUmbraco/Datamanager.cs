d0356462835d38d1ddf51eafe1893b578e37c530
Roman Parker <Roman@parker.pm>
2022-10-31 17:06:39 +0100

    initialized project

---
+++ CMSUmbraco/Datamanager.cs
@@ -0,0 +1,35 @@
+﻿namespace CMSUmbraco
+{
+    public class Root
+    {
+        public List<Station> Stations { get; set; }
+    }
+    public class Station
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public Coordinates coordinates { get; set; }
+        public double rating { get; set; }  
+        public bool paid { get; set; }  
+
+
+    }
+
+    public class Coordinates
+    {
+        public double latitute { get; set; }
+        public double lonitute { get; set; }
+    }
+    public class Datamanager
+    {
+        public static async Task<Root> GetStationsAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            Root? data = await client.GetFromJsonAsync<Root>("http://gaddr.co/ios/stations");
+            return data;
+        }
+    }
+}
---
+++ CMSUmbraco/Views/Partials/partialStations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMSUmbraco.Views.Partials
+{
+    public class partialStationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
